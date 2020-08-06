42638b37a4322414d3ce633f3d5c467b9e9ee97d
Akash Gupta <ag.gupta730@gmail.com>
2020-08-06 15:57:18 +0530

    Update Program.cs
    Adding comments

--- Adjacency_List_Implementation/Program.cs
+++ Adjacency_List_Implementation/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace DijkstraWithAdjList
 {
@@ -6,18 +6,28 @@ namespace DijkstraWithAdjList
     {
         static void Main(string[] args)
         {
+            /*Main function for Algorithm*/
+
             Console.WriteLine("Enter Number of vertices:");
             int vertices = int.Parse(Console.ReadLine());
+
+            /*Create AdjacencyList of size equal to number of vertices*/
             AdjacencyList adjList = new AdjacencyList(vertices);
+
+            /*Function is called for filling the AdjacencyList*/
             FillEdges(adjList);
             adjList.printAdjList();
+
             Console.WriteLine("Enter Source vertex:");
             int source =  int.Parse(Console.ReadLine());
+
+            /*Call Dijkstra algorithm*/
             Dijkstra dijkstra = new Dijkstra();
             dijkstra.CallDijkstra(adjList,source);
         }
-
+        
         static void FillEdges(AdjacencyList adjList){
+            /*Function is called for filling the AdjacencyList*/
             while(true){
                 Console.WriteLine("Enter an edge with weight or 'q' to quit");
                 var input = Console.ReadLine();
@@ -28,6 +38,8 @@ namespace DijkstraWithAdjList
                     int start = int.Parse(input);
                     int end = int.Parse(Console.ReadLine());
                     int weight = int.Parse(Console.ReadLine());
+
+                    /*Add edges. Since this is undirected graph so we need to add source->destination as well as destination->source*/
                     adjList.AddEdge(start,end,weight);
                     adjList.AddEdge(end,start,weight);
                 }
