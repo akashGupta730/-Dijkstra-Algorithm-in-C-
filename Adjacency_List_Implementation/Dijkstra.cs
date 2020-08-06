905f7f0ca67454793e44eaa3292ee52f6fa41e2d
Akash Gupta <ag.gupta730@gmail.com>
2020-08-06 15:56:55 +0530

    Update Dijkstra.cs
    Adding comments

--- Adjacency_List_Implementation/Dijkstra.cs
+++ Adjacency_List_Implementation/Dijkstra.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 namespace DijkstraWithAdjList
 {
@@ -8,6 +8,9 @@ namespace DijkstraWithAdjList
         {
             var minDist = int.MaxValue;
             var minIndex = -1;
+
+            /*Find vertex with minimum distance which is not included in shortest path*/
+            /*Can be optimized using min Heap*/
             for (int index = 0; index < vertices; index++)
             {
                 if (isIncluded[index] == false && dist[index] < minDist)
@@ -22,24 +25,37 @@ namespace DijkstraWithAdjList
         public void CallDijkstra(AdjacencyList adjList, int source)
         {
             int vertices = adjList.GetNumberOfVertices();
+
+            /*Dist will contain shortest distance for each vertex from source*/
             var dist = new int[vertices];
+
+            /*isIncluded[i ] will true if vertex i is included in shortest path*/
             var isIncluded = new bool[vertices];
+
+            /*Initialise all distnace with infinity and isIncluded false*/
             for (int i = 0; i < vertices; i++)
             {
                 dist[i] = int.MaxValue;
                 isIncluded[i] = false;
             }
+
+            /*Distance of source vertex from itself is 0*/
             dist[source] = 0;
 
+            /*Find Shortest path for every vertex*/
             for (int count = 0; count < vertices; count++)
             {
+                /*Pick vertex with minimum distance from source and not included in shortest path*/
                 int currentVertex = minDistanceVertices(dist, isIncluded, vertices);
                 isIncluded[currentVertex] = true;
+
+                /*Get Linked List of outward edges from currentVertex*/
                 LinkedList<Tuple<int, int>> list = adjList.getLinkedListwithSpeicficIndex(currentVertex);
                 foreach (Tuple<int, int> edge in list)
                 {
                     int dest = edge.Item1;
                     int value = edge.Item2;
+                    /*update distance if its not included in shortest path and path distance from source to this vertex is less than current distance*/
                     if (isIncluded[dest] == false && dist[currentVertex] != int.MaxValue && dist[currentVertex] + value < dist[dest])
                     {
                         dist[dest] = dist[currentVertex] + value;
@@ -47,6 +63,7 @@ namespace DijkstraWithAdjList
                 }
             }
 
+            /*Print shortest path*/
             for (int j = 0; j < vertices; j++)
             {
                 Console.WriteLine($"distance of vertex {j} from source index {source} is: {dist[j]}");
