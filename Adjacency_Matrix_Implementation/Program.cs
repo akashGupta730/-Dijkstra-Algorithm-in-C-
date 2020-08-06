93887288676cc3cbe146295e2c30a6d9f7c9194f
Akash Gupta <ag.gupta730@gmail.com>
2020-08-06 16:02:18 +0530

    Update Program.cs
    Adding Comments

--- Adjacency_Matrix_Implementation/Program.cs
+++ Adjacency_Matrix_Implementation/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Dijkstra
@@ -8,6 +8,8 @@ namespace Dijkstra
         public int minDistanceVertices(int[] dist, bool[] isIncluded,int vertices){
             var minDist=int.MaxValue;
             var minIndex=-1;
+
+            /*Find vertex with minimum distance which is not included in shortest path*/
             for(int index=0;index<vertices;index++){
                 if(isIncluded[index]==false && dist[index]<minDist){
                     minDist=dist[index];
@@ -18,22 +20,37 @@ namespace Dijkstra
         }
         public void dijkstra(int [,] graph,int source){
             var vertices = graph.GetLength(0);
+
+            /*Dist will contain shortest distance for each vertex from source*/
             var dist = new int[vertices];
+
+            /*isIncluded[i ] will true if vertex i is included in shortest path*/
             var isIncluded = new bool[vertices];
+
+            /*Initialise all distnace with infinity and isIncluded false*/
             for(int i=0;i<vertices;i++){
                 dist[i]=int.MaxValue;
                 isIncluded[i]=false;
             }
+
+            /*Distance of source vertex from itself is 0*/
             dist[source]=0;
+
+            /*Find Shortest path for every vertex*/
             for(int count =0; count<vertices;count++){
+                /*Pick vertex with minimum distance from source and not included in shortest path*/
                 int currentVertex = minDistanceVertices(dist,isIncluded,vertices);
                 isIncluded[currentVertex] = true;
+
                 for(int i=0;i<vertices;i++){
+                     /*update distance if there is path from source to dest and its not included in shortest path and path distance from source to this vertex is less than current distance*/
                     if(graph[currentVertex,i]!=0 && isIncluded[i]==false && dist[currentVertex]!=int.MaxValue && dist[currentVertex]+graph[currentVertex,i]<dist[i]){
                         dist[i]=dist[currentVertex]+graph[currentVertex,i];
                     }
                 }
             }
+
+            /*Print shortest path*/
             for(int j =0 ;j< vertices;j++){
                 Console.WriteLine($"distance of vertex {j} from source index {source} is: {dist[j]}");
             }
@@ -41,7 +58,11 @@ namespace Dijkstra
 
         static void Main(string[] args)
         {
+             /*Main function for Algorithm*/
+             /*Create Adj Matrix*/
             var graph = new [,] {{0,5,12},{5,0,3},{12,3,0}};
+
+            /*Call Dijkstra algorithm*/
             Program p = new Program();
             p.dijkstra(graph,0);
         }
