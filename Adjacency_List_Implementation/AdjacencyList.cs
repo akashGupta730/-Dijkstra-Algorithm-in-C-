c5faef51140bb05f0293abd12f2d97ece622cf57
Akash Gupta <ag.gupta730@gmail.com>
2020-08-06 15:56:29 +0530

    Update AdjacencyList.cs
    Adding comments

--- Adjacency_List_Implementation/AdjacencyList.cs
+++ Adjacency_List_Implementation/AdjacencyList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace DijkstraWithAdjList
@@ -7,10 +7,12 @@ namespace DijkstraWithAdjList
     {
         LinkedList<Tuple<int, int>>[] adjList;
 
-        //In constructor create an empty linked list of size equal to number of vertices
+        /*In constructor create an empty linked list of size equal to number of vertices*/
         public AdjacencyList(int vertices)
         {
             adjList = new LinkedList<Tuple<int, int>>[vertices];
+
+            /*create list for each vertex*/
             for(int i=0;i< vertices; i++)
             {
                 adjList[i] = new LinkedList<Tuple<int, int>>();
@@ -19,25 +21,29 @@ namespace DijkstraWithAdjList
 
         public void AddEdge(int start,int end,int weight)
         {
+            /*Add edges in adj list from front: Insertion O(1)*/
             adjList[start].AddFirst(new Tuple<int,int>(end,weight));
         }
 
         public int GetNumberOfVertices()
         {
+            /*return length of adjList*/
             return adjList.Length;
         }
 
         public LinkedList<Tuple<int, int>> getLinkedListwithSpeicficIndex(int index)
         {
+            /*Returns the Linked List of outward edges from a vertex*/
             LinkedList<Tuple<int, int>> edgeList
                                = new LinkedList<Tuple<int, int>>(adjList[index]);
 
             return edgeList;
         }
 
-        // Prints the Adjacency List
+        
         public void printAdjList()
         {
+            /*Prints the Adjacency List*/
             int i = 0;
 
             foreach (LinkedList<Tuple<int, int>> list in adjList)
